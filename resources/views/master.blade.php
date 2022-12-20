2bc0cd6cdc83b6ca90b9b26fdf197a3f91e69291
mukesh <you@example.com>
2022-12-20 11:10:18 +0545

    Header, Footer and Login Page Finish

---
+++ resources/views/master.blade.php
@@ -0,0 +1,30 @@
+<!Doctype html>
+	<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title> E-Commerce Project </title>
+		<script
+  src="https://code.jquery.com/jquery-3.6.2.min.js"
+  integrity="sha256-2krYZKh//PcchRtd+H+VyyQoZ/e3EcrkxhM8ycwASPA="
+  crossorigin="anonymous"></script>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		{{View::make('header')}}
+		@yield('content')
+		{{View::make('footer')}}
+	</body>
+	<style>
+		.custom-login{
+			height: 500px;
+			padding-top: 100px;
+		}
+		.bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+	</style>
+	</html>
